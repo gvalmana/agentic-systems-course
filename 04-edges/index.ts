230d2c2ceae10ff201362ce2c37ebaba4425e371
Derian Rosado <derian.rosado@alegra.com>
2025-07-08 00:59:43 -0600

    Refactor AI-powered content enhancement handler in index.ts to improve clarity and functionality by renaming to aiTextProcessorHandler and updating the import path.

--- 04-edges/index.ts
+++ 04-edges/index.ts
@@ -1,10 +1,10 @@
 import { handler as basicHandler } from "./01-basic";
-import { handler as aiPoweredContentEnhancementPipelineHandler } from "./02";
+import { handler as aiTextProcessorHandler } from "./02-textProcessor";
 
 // Basic example
 basicHandler();
 
 // AI-Powered Content Enhancement Pipeline
-aiPoweredContentEnhancementPipelineHandler(
+aiTextProcessorHandler(
   "Ayer fuimos a la plalla aver si encontrabamos algun lugar donde comieramos algo rico. No abia muchas personas por que estaba nublado, pero iguál nos divertimos mucho. Mi ermano jugo con la pelota mientras yo leia un libro que me prestaron en la viclioteca. Al final del día, nos regresamos cansados pero felis."
 );
